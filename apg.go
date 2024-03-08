61ca9af22ac533bf2506a50fea253278de73f05b
Winni Neessen <wn@neessen.net>
2024-03-08 10:21:57 +0100

    Refactor Generator struct and New function in apg.go
    Removed the unused 'charRange' field from the Generator struct. Also, renamed the parameter in the New function from 'c' to 'config' for better clarity and understanding of its purpose.

--- apg.go
+++ apg.go
@@ -5,15 +5,13 @@ const VERSION = "2.0.0"
 
 // Generator is the password generator type of the APG package
 type Generator struct {
-	// charRange is the range of character used for the
-	charRange string
 	// config is a pointer to the apg config instance
 	config *Config
 }
 
 // New returns a new password Generator type
-func New(c *Config) *Generator {
+func New(config *Config) *Generator {
 	return &Generator{
-		config: c,
+		config: config,
 	}
 }
