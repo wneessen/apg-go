0bb1b6c09b35961f1e06d72a92001223e89d5857
Winni Neessen <wn@neessen.net>
2024-03-25 11:06:54 +0100

    Add grouping functionality and corresponding tests
    Implemented a new function 'GroupCharsForMobile' in the 'apg' package, which groups characters in a mobile-friendly manner based on their Unicode category. Accompanying tests for this function have also been created in 'grouping_test.go'. This update enhances password readability on mobile devices.

---
+++ grouping.go
@@ -0,0 +1,23 @@
+package apg
+
+import "unicode"
+
+// GroupCharsForMobile takes a given string of characters and groups them in a mobile-friendly
+// manner. The grouping is based on the following precedense: uppercase, lowercase, numbers
+// and special characters. The grouped string is then returned.
+func GroupCharsForMobile(chars string) string {
+	var uppers, lowers, numbers, others []rune
+	for _, char := range chars {
+		switch {
+		case unicode.IsUpper(char):
+			uppers = append(uppers, char)
+		case unicode.IsLower(char):
+			lowers = append(lowers, char)
+		case unicode.IsNumber(char):
+			numbers = append(numbers, char)
+		default:
+			others = append(others, char)
+		}
+	}
+	return string(uppers) + string(lowers) + string(numbers) + string(others)
+}
